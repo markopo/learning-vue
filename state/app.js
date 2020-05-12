cb76a350be0a5d7d6dfba61586baf98920ef6cdb
Marko <marko_poikkimaki@hotmail.com>
2020-05-12 11:07:28 +0200

    shared state

---
+++ state/app.js
@@ -0,0 +1,21 @@
+let store = {
+    user: {
+        name: 'John Doe'
+    }
+};
+
+new Vue({
+    el: '#one',
+    data: {
+        foo: 'bar',
+        shared: store
+    }
+});
+
+new Vue({
+    el: '#two',
+    data: {
+        foo: 'other bar',
+        shared: store
+    }
+});
